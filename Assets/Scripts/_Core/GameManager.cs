0fbba90ee78ca6156b577a8c50253e3f87af2e6b
Mitchell Opitz <mitchell@brainfart.studio>
2026-08-16 06:52:54 -0500

    feat - Transition Dying state to GameOver on a timer

--- Assets/Scripts/_Core/GameManager.cs
+++ Assets/Scripts/_Core/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using BFTools.Core.EventBus;
 using UnityEngine;
 
@@ -24,6 +25,9 @@ public class GameManager : MonoBehaviour
     private static GameManager instance;
     public static GameManager Instance => instance;
 
+    [Tooltip("Time to hold in the Dying state before moving to GameOver. Placeholder until death visuals (particles, tile break-fall) are wired up")]
+    [SerializeField] private float deathSequenceDuration = 1.5f;
+
     public GameState CurrentState { get; private set; } = GameState.Playing;
 
     private void Awake()
@@ -40,6 +44,13 @@ public class GameManager : MonoBehaviour
     private void OnPlayerDeath(PlayerDeathEvent e)
     {
         ChangeState(GameState.Dying);
+        StartCoroutine(EndDeathSequence());
+    }
+
+    private IEnumerator EndDeathSequence()
+    {
+        yield return new WaitForSeconds(deathSequenceDuration);
+        ChangeState(GameState.GameOver);
     }
 
     public void ChangeState(GameState newState)
