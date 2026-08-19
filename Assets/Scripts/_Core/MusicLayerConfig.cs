384a8cad46519f97dc1ab2b96f7efd87daddcc45
Mitchell Opitz <mitchell@brainfart.studio>
2026-08-18 22:58:19 -0500

    feat - Add MusicLayerConfig for difficulty-driven music layers

---
+++ Assets/Scripts/_Core/MusicLayerConfig.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MusicLayerConfig", menuName = "Audio/Music Layer Config")]
+public class MusicLayerConfig : ScriptableObject
+{
+    [Serializable]
+    public class MusicLayer
+    {
+        public AudioClip clip;
+
+        [Tooltip("Difficulty at which this layer starts fading in")]
+        public float startDifficulty;
+
+        [Tooltip("Difficulty at which this layer reaches its max volume")]
+        public float maxDifficulty;
+
+        [Range(0f, 1f)]
+        [Tooltip("Volume this layer reaches once past maxDifficulty")]
+        public float maxVolume = 1f;
+    }
+
+    public List<MusicLayer> layers = new List<MusicLayer>();
+}
